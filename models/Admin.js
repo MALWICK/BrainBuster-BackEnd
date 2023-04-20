16d7eebe65333728059897b0ea789504be75562b
MALWICK <batahumphrey66@gmail.com>
2023-04-20 16:39:55 +0100

    create Admin database table

---
+++ models/Admin.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("");
+const Admin = sequelize.define("Admin", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  fullName : DataTypes.STRING,
+  emailAddress: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  Password: DataTypes.STRING,
+  is_SuperAdmin: false,
+});
