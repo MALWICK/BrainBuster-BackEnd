7f928526f0ef89ebcd23587242d7ab7dae80f126
NNVG <g696607013@gmail.com>
2023-04-22 08:47:41 +0100

    modify bin file

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const cors = require("cors");
 const dotEnv = require("dotenv");
 const specs = require("./services/swagger");
 const swaggerUi = require("swagger-ui-express");
@@ -12,6 +11,7 @@ dotEnv.config();
 const usersRouter = require("./routes/usersRoutes");
 
 const app = express();
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -22,7 +22,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(cors());
 app.use("/user", usersRouter);
 
 app.use(
@@ -30,6 +29,12 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(specs, { explorer: true })
 );
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
